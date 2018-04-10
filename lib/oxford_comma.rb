3e53a34abb4efec6f9b362ffefab61d8b8c6767c
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-10 21:50:58 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,13 @@
 def oxford_comma(array)
+  my_new_array = []
+  i = 0
+  while i < array.length - 1
+    my_new_array.push(array[i])
+    i += 1
+  end
   
-  array.join(", ")
+  i = array.length - 1
+  my_new_array.push("and #{array[i]}")
+  
+  my_new_array.join(", ")
 end
